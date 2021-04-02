54a9d1d2e77c5d66d18390c1bf02b7479a40f252
Brian_Izaki <bkizaki@outlook.com>
2021-04-02 11:29:04 -0300

    feat: Header component

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,12 +1,49 @@
 import styled from "styled-components";
+import logo from "../../assets/logo.svg";
 
-
-const Header = styled.header`
-  color: ${(props) => props.theme.mainColor};
-  font-size: 0.85rem;
-  line-height: 1.5rem;
-  font-weight: 600;
-  margin: 10px;
+const HeaderContainer = styled.header`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 24px 32px;
 `;
 
-export default Header;
+const ButtonExit = styled.a`
+  color: ${(props) => props.theme.mainColor};
+  text-decoration: none;
+  font-size: 1rem;
+  line-height: 1.5rem;
+  font-weight: 600;
+  padding: 0 5px;
+
+  &::after {
+    content: "";
+    display: block;
+    width: 0%;
+    height: 2px;
+    transition: 0.5s;
+  }
+
+  &:hover {
+    &::after {
+      background-color: ${(props) => props.theme.mainColor};
+      width: 100%;
+    }
+  }
+`;
+
+export default function Header() {
+  return (
+    <HeaderContainer>
+      <div>
+        <img src={logo} alt="Logo do Navdex (Nave.rs)" />
+      </div>
+
+      <div />
+
+      <div>
+        <ButtonExit href="#">Sair</ButtonExit>
+      </div>
+    </HeaderContainer>
+  );
+}
