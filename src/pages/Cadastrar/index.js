d48b50095f8d48b5a9e02be15bc5c471be5fd743
Brian_Izaki <bkizaki@outlook.com>
2021-04-05 14:57:16 -0300

    feat: pagina utilizando formulario para cadastro

--- src/pages/Cadastrar/index.js
+++ src/pages/Cadastrar/index.js
@@ -1,11 +1,19 @@
+import { useState } from "react";
+import FormNaverProfile from "../../components/FormNaverProfile";
 import Header from "../../components/Header";
+import ModalInformation from "../../components/Modal/Information";
 
 export default function Cadastrar() {
+  const [isOpenModal, setIsOpenModal] = useState(true)
+
   return (
     <>
       <Header />
-      <div>Página de Cadastro</div>
-
+      <FormNaverProfile title="Adicionar Naver"/>
+      <ModalInformation isOpenModal={isOpenModal} setIsOpenModal={setIsOpenModal} >
+        <h1>Naver criado</h1>
+        <p>Naver criado com sucesso</p>
+      </ModalInformation>
     </>
   )
 }
